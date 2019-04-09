fe6fa9a5df21db55348e341fc0f571b235f55907
kks96500 <kks96500@gmail.com>
2019-04-09 12:59:09 +0530

    updated

--- faceExtract/face_extract.py
+++ faceExtract/face_extract.py
@@ -7,9 +7,10 @@ import time
 from random import randint
 import sys
 import os
+import face_recognition
 import glob
 
-
+#taking photos from camera
 capture_duration=0.3
 start_time=time.time()
 cap=cv.VideoCapture(0)
@@ -24,8 +25,8 @@ while(int(time.time() - start_time) < capture_duration):
 cap.release()
 #cv.destroyAllWindows()
       
+#detecting and extracting the faces
 CASCADE="/home/kks96500/.local/lib/python3.6/site-packages/cv2/data/haarcascade_frontalface_alt.xml"
-
 FACE_CASCADE=cv.CascadeClassifier(CASCADE)
 
 def detect_faces(image_path):
@@ -51,7 +52,49 @@ if __name__ == "__main__":
 	if not "Extracted" in os.listdir("."):
 		os.mkdir("Extracted")
 	for img in glob.glob('*.jpg'):
-		detect_faces(img)
+			detect_faces(img)
+for i in glob.glob('*.jpg'):
+	os.remove(i)
+
+
+
+'''#recognising faces
+
+# make a list of all the available images
+images = os.listdir('images')
+extract=os.listdir('Extracted')
+# load your image
+for img in extract:
+	image_to_be_matched = face_recognition.load_image_file(img)
+
+	# encoded the loaded image into a feature vector
+	image_to_be_matched_encoded = face_recognition.face_encodings(image_to_be_matched)[0]
+	# iterate over each image
+	for image in images:
+    		# load the image
+    		current_image = face_recognition.load_image_file("images/" + image)
+    		# encode the loaded image into a feature vector
+    		current_image_encoded = face_recognition.face_encodings(current_image)[0]
+    		#match your image with the image and check if it matches
+    		result = face_recognition.compare_faces([image_to_be_matched_encoded], current_image_encoded)
+    		#check if it was a match
+    		if result[0] == True:
+        		print("Matched: "+image)
+    		else:
+        		print("Not matched: " + image)'''
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
